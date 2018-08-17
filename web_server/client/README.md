e490ef5d2e392d903c58c51697c9cdfff7cd8e32
Jack <hjin96@bu.edu>
2018-08-16 22:38:01 -0700

    deleted node_modules and created backend

--- web_server/client/README.md
+++ web_server/client/README.md
@@ -1,8 +1,4 @@
 # News Recommandation System using ReactJS
 
-## Install and initialize react app using create-react-app
 
-```
-sudo npm install -g create-react-app
-```
 
---
+++ web_server/server/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+
+var index = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use('/static', express.static(path.join(__dirname, '../client/build/static/')));
+
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  res.status(404);
+});
+
+module.exports = app;
