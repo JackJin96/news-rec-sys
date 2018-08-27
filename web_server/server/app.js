c70e88ba2eb5251219568cf577ea88faab455b9d
Jack <hjin96@bu.edu>
2018-08-27 17:18:04 -0400

    added cross origin header

--- web_server/server/app.js
+++ web_server/server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const index = require('./routes/index');
+const news = require('./routes/news');
 
 const app = express();
 
@@ -10,7 +11,14 @@ app.set('views', path.join(__dirname, '../client/build'));
 app.set('view engine', 'jade');
 app.use('/static', express.static(path.join(__dirname, '../client/build/static')));
 
+app.all('*', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+});
+
 app.use('/', index);
+app.use('/news', news);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
