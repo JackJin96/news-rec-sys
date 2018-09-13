121001615f20fe4d4ab10e4505b6a2ffb574f35b
Jack <hjin96@bu.edu>
2018-09-13 16:30:16 -0400

    added CloudAMQP and test

---
+++ backend_server/utils/cloudAMQP_client_test.py
@@ -0,0 +1,18 @@
+from cloudAMQP_client import CloudAMQPClient
+
+CLOUDAMQPURL = "amqp://wgywavma:LknOYsnIwoWwzGRI9s5qd3J4tPjxKW7J@termite.rmq.cloudamqp.com/wgywavma"
+TEST_QUEUE_NAME = "test"
+
+def test_basic():
+    client = CloudAMQPClient(CLOUDAMQPURL, TEST_QUEUE_NAME)
+
+    sentMsg = {"test" : "test"}
+    client.sendMessage(sentMsg)
+
+    receivedMsg = client.getMessage()
+
+    assert sentMsg = receivedMsg
+    print("test_basic passed!")
+
+if __name__ == "__main__":
+    test_basic()
